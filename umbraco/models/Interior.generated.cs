c039a1fd3b3933e98660e2ae3dfd343307bb67d8
Muhannad-Satouf <muhannad.satouf@greatit.se>
2024-04-02 19:19:38 +0200

    Add new interior page and remove the old and fix a bug

--- umbraco/models/Interior.generated.cs
+++ umbraco/models/Interior.generated.cs
@@ -18,7 +18,7 @@ using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Web.Common.PublishedModels
 {
-	/// <summary>Interior Page</summary>
+	/// <summary>Interior</summary>
 	[PublishedModel("interior")]
 	public partial class Interior : PublishedContentModel, IPageProperties, IVisibilityProperties
 	{
